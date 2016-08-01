65dc19a17aad55981af102dd98f87dc8f1460beb
Michael Johnson <msjohnso2003@gmail.com>
2016-08-01 11:53:28 -0500

    Initial Commit

---
+++ Business Finder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Business Finder
+//
+//  Created by Michael Johnson on 8/1/16.
+//  Copyright © 2016 Michael Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
